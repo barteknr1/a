7e5610585265ae5aa6ba991f6eaa56a0f12b7421
Bartek <Barteknr1@gmail.com>
2023-03-04 11:56:31 +0100

    Update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+
+galleryItems.forEach((item) => {
+    const galleryItem = `
+        <a class="gallery__item"
+        href ="${item.original}">
+        <img class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}" /></a>`
+    gallery.insertAdjacentHTML('beforeend', galleryItem);
+})
+
+let lightbox = new SimpleLightbox('.gallery__item', { 
+    captionDelay: 250,
+    captionsData: 'alt',
+ });
